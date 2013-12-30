0a0ef9094f0470a2472c8e32fbf86f5d2dc64fae
Pradeep G <itsprdp@gmail.com>
2013-12-30 12:26:30 +0530

    Added few yelp restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Restaurant.create(name: "Chutneys, Banjara Hills", zomato_url: "http://www.zomato.com/hyderabad/chutneys-banjara-hills/reviews", burrp_url: "http://hyderabad.burrp.com/listing/chutneys_banjara-hills_hyderabad_restaurants/121415188__UR__reviews") if !Restaurant.find_by_name('Chutneys, Banjara Hills').present?
-
-Restaurant.create(name: "Indijoe, Banjara Hills", zomato_url: "http://www.zomato.com/hyderabad/indijoe-banjara-hills/reviews", burrp_url: "http://hyderabad.burrp.com/listing/indijoe_banjara-hills_hyderabad_restaurants/155472554__UR__reviews")
-Restaurant.create(name: "Indijoe, Banjara Hills", zomato_url: "http://www.zomato.com/hyderabad/paradise-food-court-paradise-circle-secunderabad/reviews", burrp_url: "http://hyderabad.burrp.com/listing/paradise-food-court_s-d-road_secunderabad_restaurants/132416277__UR__reviews")
-Restaurant.create(name: "Sahib Sindh Sultan, Banjara Hills", zomato_url: "http://www.zomato.com/hyderabad/sahib-sind-sultan-banjara-hills/reviews", burrp_url: "http://hyderabad.burrp.com/listing/sahib-sindh-sultan_banjara-hills_hyderabad_restaurants/187484722__UR__reviews")
+Restaurant.create(name: "Indijoe, Banjara Hills", zomato_url: "http://www.zomato.com/hyderabad/indijoe-banjara-hills/reviews", burrp_url: "http://hyderabad.burrp.com/listing/indijoe_banjara-hills_hyderabad_restaurants/155472554__UR__reviews") if !Restaurant.find_by_name('Indijoe, Banjara Hills').present?
+Restaurant.create(name: "Paradise Food Court, Secunderabad", zomato_url: "http://www.zomato.com/hyderabad/paradise-food-court-paradise-circle-secunderabad/reviews", burrp_url: "http://hyderabad.burrp.com/listing/paradise-food-court_s-d-road_secunderabad_restaurants/132416277__UR__reviews") if !Restaurant.find_by_name('Paradise Food Court, Secunderabad').present?
+Restaurant.create(name: "Sahib Sindh Sultan, Banjara Hills", zomato_url: "http://www.zomato.com/hyderabad/sahib-sind-sultan-banjara-hills/reviews", burrp_url: "http://hyderabad.burrp.com/listing/sahib-sindh-sultan_banjara-hills_hyderabad_restaurants/187484722__UR__reviews") if !Restaurant.find_by_name('Sahib Sindh Sultan, Banjara Hills').present?
+Restaurant.create(name: "Porto's Bakery", yelp_url: "http://www.yelp.com/biz/portos-bakery-glendale") if !Restaurant.find_by_name("Porto's Bakery").present?
+Restaurant.create(name: "Raffis Place", yelp_url: "http://www.yelp.com/biz/raffis-place-glendale-2") if !Restaurant.find_by_name("Raffis Place").present?
+Restaurant.create(name: "Skaf's Lebanese Cuisine", yelp_url: "http://www.yelp.com/biz/skafs-lebanese-cuisine-glendale") if !Restaurant.find_by_name("Skaf's Lebanese Cuisine").present?
+Restaurant.create(name: "Mario's Italian Deli & Market", yelp_url: "http://www.yelp.com/biz/marios-italian-deli-and-market-glendale") if !Restaurant.find_by_name("Mario's Italian Deli & Market").present?
+Restaurant.create(name: "Eden Burger Bar", yelp_url: "http://www.yelp.com/biz/eden-burger-bar-glendale") if !Restaurant.find_by_name("Eden Burger Bar").present?
+Restaurant.create(name: "Foxy's Restaurant", yelp_url: "http://www.yelp.com/biz/foxys-restaurant-glendale-3") if !Restaurant.find_by_name("Foxy's Restaurant").present?
