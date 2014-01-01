b24ba22b532e90cf6c5021e09bf053a0e909df7a
Ravi Bhim <ravi.bhim@yahoo.com>
2014-01-01 14:22:52 +0530

    normalize notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,4 +4,13 @@ class Note < ActiveRecord::Base
   attr_accessible :item, :rating, :review_id, :user_id
 
   validates_presence_of :user_id
+  before_save :normalize_item!
+
+  def normalize_item!
+    self.item = normalized_item
+  end
+
+  def normalized_item
+    item.split(" ").collect(&:downcase).collect(&:capitalize).join(' ')
+  end
 end
