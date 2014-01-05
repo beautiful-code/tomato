e493dc873783372b3345823bf7c4bda09b7fe388
Ravi Bhim <ravi.bhim@yahoo.com>
2014-01-05 14:51:50 +0530

    parameters form

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -31,7 +31,90 @@ class Parameter < ActiveRecord::Base
       "values" => ["Disliked it", "Liked it", "Loved it"],
       "category" => 'restaurant'
     },
+    "parking" => {
+      "label" => "Parking",
+      "values" => ["Easy", "Difficult"],
+      "category" => "restaurant"
+    },
+    "menu_options" => {
+      "label" => "Menu Options",
+      "values" => ["Disliked it", "Liked id", "Loved it"],
+      "category" => "restaurant"
+    },
+    "meals" => {
+      "label" => "Menu Options",
+      "values" => ["Brunch", "Breakfast", "Lunch", "Snack", "Dinner"],
+      "category" => "context"
+    },
+    "portions" => {
+      "label" => "Portion size/Filling",
+      "values" => ["Too large", "too small", "just enough"],
+      "category" => "food"
+    },
+    "noise" => {
+      "label" => "Noise",
+      "values" => ["Disliked it", "Liked id", "Loved it"],
+      "category" => "restaurant"
+    },
+    "user" => {
+      "label" => "User",
+      "values" => ["First time", "Casual repeat", "Regular"],
+      "category" => "context"
+    },
+    "occasion" => {
+      "label" => "Occasion",
+      "values" => ["Date", "Family", "Friends", "Alone", "Unplanned"],
+      "category" => "context"
+    },
+    "healthy" => {
+      "label" => "Healthy",
+      "values" => ["Unhealthy", "Healthy"],
+      "category" => "food"
+    },
+    "attentiveness_of_staff" => {
+      "label" => "Attentiveness of Staff",
+      "values" => ["Not attentive", "Attentive"],
+      "category" => "service"
+    },
+    "time_to_drive" => {
+      "label" => "Time taken to drive to restaurant",
+      "values" => ["Short", "Long"],
+      "category" => "extrensic"
+    },
+    "hygene" => {
+      "label" => "Hygene",
+      "values" => ["Unhygenic", "Hygenic"],
+      "category" => "restaurant"
+    },
+    "free_items" => {
+      "label" => "Free Items",
+      "values" => ["No", "Yes"],
+      "category" => "restaurant"
+    },
+    "complementary_available" => {
+      "label" => "Availability of complementary products",
+      "values" => ["No", "Yes"],
+      "category" => "extrensic"
+    },
+    "nearby_places_influence" => {
+      "label" => "Nearby places influence",
+      "values" => ["No", "Yes"],
+      "category" => "extrensic"
+    },
+
   }
+
+  ['restaurant', 'service', 'food', 'context', 'extrensic'].each do |category|
+    define_singleton_method "#{category}_features" do
+      FEATURES.select {|k,v| v["category"] == category }
+    end
+  end
+
+=begin
+  def self.restaurant_features
+    FEATURES.select {|k,v| v["category"] == "restaurant" }
+  end
+=end
 
   FEATURES.each do |key, info|
     define_method key do
