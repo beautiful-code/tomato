baaefb502aec383693a18a4d60732bd4317bcb0b
Ravi Bhim <ravi.bhim@yahoo.com>
2014-01-05 10:44:38 +0530

    some more parameters

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -8,12 +8,29 @@ class Parameter < ActiveRecord::Base
   FEATURES = {
     "courtesy" => {
       "label" => "Courtesy of Staff",
-      "values" => ["Rude", "Neutral", "Courteous/Friendly"]
+      "values" => ["Rude", "Neutral", "Courteous/Friendly"],
+      "category" => "service"
     },
     "queue_at_counter" => {
       "label" => "Queue at Counter",
-      "values" => ["Long", "Quick Moving", "Short/No Queue"]
-    }
+      "values" => ["Long", "Quick Moving", "Short/No Queue"],
+      "category" => "restaurant"
+    },
+    "wait_time_after_ordering" => {
+      "label" => "Wait time after ordering",
+      "values" => ["Long", "Short"],
+      "category" => 'restaurant'
+    },
+    "price" => {
+      "label" => "Price",
+      "values" => ["Value for money", "Slightly Expensive", "Very Expensive"],
+      "category" => 'food'
+    },
+    "ambience" => {
+      "label" => "Ambience",
+      "values" => ["Disliked it", "Liked it", "Loved it"],
+      "category" => 'restaurant'
+    },
   }
 
   FEATURES.each do |key, info|
