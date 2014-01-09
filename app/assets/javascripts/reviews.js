d7f355e85a11b62d1a8d6b063c33c7a98bb2e358
lnreddy <simhaece@gmail.com>
2014-01-09 14:41:10 +0530

    dish note select popover

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,4 +1,42 @@
-$(document).ready(function() {
-  // Uncheckable radio boxes
-  $('#parameters input[type="radio"]').uncheckable_radio();
+$(document).ready(function () {
+
+    // Hacking popover to support callback
+    var tmp = $.fn.popover.Constructor.prototype.show;
+    $.fn.popover.Constructor.prototype.show = function () {
+        tmp.call(this);
+        if (this.options.callback) {
+            this.options.callback();
+        }
+    };
+
+
+    // Uncheckable radio boxes
+    $('#parameters input[type="radio"]').uncheckable_radio();
+
+    function getSelectionText() {
+        var text = "";
+        if (window.getSelection) {
+            text = window.getSelection().toString();
+        } else if (document.selection && document.selection.type != "Control") {
+            text = document.selection.createRange().text;
+        }
+
+        if (text != '') {
+            $('.desc').popover('destroy');
+            $('.desc').popover({
+                html: true,
+                content: $('#new_note_form').html(),
+                callback: function () {
+                    $('.popover .new-note-form #note_item').val(text);
+                    $('.new-note-form').notesManager();
+                }
+            });
+        }
+    }
+
+
+    $(document.body).bind('mouseup', function () {
+        getSelectionText();
+    });
+
 });
